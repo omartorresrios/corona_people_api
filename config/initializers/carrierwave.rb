c0bfd0c31cab4d508cbc2b2918e27b911ca83ee5
Omar Torres Rios <omar.torres@rappi.com>
2020-03-28 11:30:58 -0500

    hide aws credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,8 +16,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => 'AKIAQQ6LFDLKFMJUNSDD',            # required
-    :aws_secret_access_key  => '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',     # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
     :region                 => 'us-east-1'                        # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'citizencoronaperu'               # required
